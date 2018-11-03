53848efb26d26235606f486af78ba640747f4851
Solgi Niloufar <N_Solgi@iranet.net>
2018-11-03 13:23:05 +0330

    week03

---
+++ week03.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 11:41:35 2018
+
+@author: N_Solgi
+"""
+
+import pandas as pd
+import numpy as np
+dtype_dict = {'bathrooms':float, 'waterfront':int, 'sqft_above':int, 'sqft_living15':float, 'grade':int, 'yr_renovated':int, 'price':float, 'bedrooms':float, 'zipcode':str, 'long':float, 'sqft_lot15':float, 'sqft_living':float, 'floors':str, 'condition':int, 'lat':float, 'date':str, 'sqft_basement':int, 'yr_built':int, 'id':str, 'sqft_lot':int, 'view':int}
