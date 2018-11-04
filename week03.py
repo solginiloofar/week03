ac0684fbc817ea5f87c6ba6d24a5c47bdcb00f5e
Solgi Niloufar <N_Solgi@iranet.net>
2018-11-04 15:55:37 +0330

    13aban

--- week03.py
+++ week03.py
@@ -8,3 +8,6 @@ Created on Sat Nov  3 11:41:35 2018
 import pandas as pd
 import numpy as np
 dtype_dict = {'bathrooms':float, 'waterfront':int, 'sqft_above':int, 'sqft_living15':float, 'grade':int, 'yr_renovated':int, 'price':float, 'bedrooms':float, 'zipcode':str, 'long':float, 'sqft_lot15':float, 'sqft_living':float, 'floors':str, 'condition':int, 'lat':float, 'date':str, 'sqft_basement':int, 'yr_built':int, 'id':str, 'sqft_lot':int, 'view':int}
+polynomial = function(feature, power){
+  return(data.frame(sapply(1:power, function(x){(feature)**x})))
+}
